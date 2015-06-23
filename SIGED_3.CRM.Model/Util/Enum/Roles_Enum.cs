f67ea82fec1048897f803864041bc684a42a1410
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2015-06-22 20:15:04 -0500

    Corrección
    Se ajusta el Id de los roles según la ultima migración

--- SIGED_3.CRM.Model/Util/Enum/Roles_Enum.cs
+++ SIGED_3.CRM.Model/Util/Enum/Roles_Enum.cs
@@ -7,8 +7,8 @@ namespace SIGED_3.CRM.Model.Util.Enum
 {
     public enum Roles_Enum : int
     {
-        AdministradorDelSistema = 13,
-        GerenteComercial = 14,
-        ClientePremium = 15
+        AdministradorDelSistema = 16,
+        GerenteComercial = 17,
+        ClientePremium = 18
     }
 }
